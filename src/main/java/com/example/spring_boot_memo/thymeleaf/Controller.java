f5e2dae3e5ba97d11dae0476cadeb85e90fb73c4
RYA234 <mryuabc@gmail.com>
2022-06-15 08:13:42 +0900

    html追加

--- src/main/java/com/example/spring_boot_memo/thymeleaf/Controller.java
+++ src/main/java/com/example/spring_boot_memo/thymeleaf/Controller.java
@@ -12,5 +12,39 @@ public class Controller {
     {
         return "index";
     }
+    @RequestMapping(value = "thymeleaf/list", method = RequestMethod.GET)
+    public String thymeleafListController()
+    {
+        return "thymeleaf/list";
+    }
 
+    @RequestMapping(value = "thymeleaf/1_menu", method = RequestMethod.GET)
+    public String thymeleaf_1_menuController()
+    {
+        return "thymeleaf/1_menu";
+    }
+
+    @RequestMapping(value = "thymeleaf/2_menu", method = RequestMethod.GET)
+    public String thymeleaf_2_menuController()
+    {
+        return "thymeleaf/2_menu";
+    }
+
+    @RequestMapping(value = "thymeleaf/3_menu", method = RequestMethod.GET)
+    public String thymeleaf_3_menuController()
+    {
+        return "thymeleaf/3_menu";
+    }
+
+    @RequestMapping(value = "thymeleaf/4_menu", method = RequestMethod.GET)
+    public String thymeleaf_4_menuController()
+    {
+        return "thymeleaf/4_menu";
+    }
+
+    @RequestMapping(value = "thymeleaf/5_menu", method = RequestMethod.GET)
+    public String thymeleaf_5_menuController()
+    {
+        return "thymeleaf/5_menu";
+    }
 }
---
+++ src/main/java/com/example/spring_boot_memo/thymeleaf/ModelForm.java
@@ -0,0 +1,7 @@
+package com.example.spring_boot_memo.thymeleaf;
+
+public class ModelForm {
+    int count;
+    String
+
+}
