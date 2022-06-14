b7d982d6cd4926ea798db053cd19560ed2b16a6e
RYA234 <mryuabc@gmail.com>
2022-06-15 07:09:11 +0900

    Initial commit

---
+++ src/main/java/com/example/spring_boot_memo/SpringBootMemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_memo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_memo/SpringBootMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_memo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
