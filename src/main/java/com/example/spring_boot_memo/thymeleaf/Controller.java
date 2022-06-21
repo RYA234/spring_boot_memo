6f62d3ad243717df07fd0f62b2c7d92dcf1c4025
RYA234 <mryuabc@gmail.com>
2022-06-21 11:25:58 +0900

    4完成

--- src/main/java/com/example/spring_boot_memo/thymeleaf/Controller.java
+++ src/main/java/com/example/spring_boot_memo/thymeleaf/Controller.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import java.lang.String;
+import java.util.ArrayList;
+import java.util.List;
+
 @org.springframework.stereotype.Controller
 public class Controller {
 
@@ -92,14 +95,64 @@ public class Controller {
     @RequestMapping(value = "thymeleaf/3_menu", params = "OK", method = RequestMethod.POST)
     public String thymeleaf_3_menuOkController(Model model,ModelForm modelForm)
     {
-
+        System.out.print("3_okのmodelFormについて");
+        System.out.print(modelForm);
         return "thymeleaf/3_menu_ok";
     }
 
     @RequestMapping(value = "thymeleaf/3_menu", params = "NG", method = RequestMethod.POST)
     public String thymeleaf_3_menuNgController(Model model,ModelForm modelForm)
     {
+        System.out.print("3_ngのmodelFormについて");
+        System.out.print(modelForm);
         return "thymeleaf/3_menu_ng";
     }
 
+    @RequestMapping(value = "thymeleaf/4_menu", params = "OK", method = RequestMethod.POST)
+    public String thymeleaf_4_menuOkController(Model model)
+    {
+        List<ModelForm> list = new ArrayList<ModelForm>();
+        ModelForm  modelForm = new ModelForm();
+        modelForm.setCount(1);
+        modelForm.setName("aaaaa");
+        list.add(modelForm);
+
+        modelForm.setCount(2);
+        modelForm.setName("ABABAAA");
+        list.add(modelForm);
+
+        modelForm.setCount(3);
+        modelForm.setName("CACAAA");
+        list.add(modelForm);
+
+        model.addAttribute("list",list);
+        System.out.print("4_okのlist_OKについて");
+        System.out.print(list);
+        return "thymeleaf/4_menu_ok";
+    }
+
+    @RequestMapping(value = "thymeleaf/4_menu", params = "NG", method = RequestMethod.POST)
+    public String thymeleaf_4_menuNgController(Model model)
+    {
+        List<ModelForm> list = new ArrayList<>();
+        ModelForm  modelForm = new ModelForm();
+        modelForm.setCount(1);
+        modelForm.setName("aaaaa");
+        list.add(modelForm);
+
+        modelForm.setCount(2);
+        modelForm.setName("ABABAAA");
+        list.add(modelForm);
+
+        modelForm.setCount(3);
+        modelForm.setName("CACAAA");
+        list.add(modelForm);
+
+        model.addAttribute(list);
+
+        System.out.print("4_okのlist_NGについて");
+        System.out.print(list);
+
+        return "thymeleaf/4_menu_ng";
+    }
 }
