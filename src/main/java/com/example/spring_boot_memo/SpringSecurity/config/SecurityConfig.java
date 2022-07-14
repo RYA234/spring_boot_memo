3c342e2e28b331eb0a0b27d6657d89f75054eb9d
RYA234 <103587568+RYA234@users.noreply.github.com>
2022-07-14 16:02:08 +0000

    プルリクエストの内容更新２

--- src/main/java/com/example/spring_boot_memo/SpringSecurity/config/SecurityConfig.java
+++ src/main/java/com/example/spring_boot_memo/SpringSecurity/config/SecurityConfig.java
@@ -1,26 +1,22 @@
 package com.example.spring_boot_memo.SpringSecurity.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 
+
+
     @Autowired
     public PasswordEncoder passwordEncoder;
-
 
     /** セキュリティの各種設定 */
     @Override
@@ -32,21 +28,21 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
                 .antMatchers("/index").permitAll()
                 .antMatchers("/login").permitAll()
                 .antMatchers("/check/access").permitAll()
-                .anyRequest().authenticated();  // 上記以外のviewはログイン前にはみれなくなる。
+                .anyRequest().authenticated();
         // ログイン処理
         http
                 .formLogin()
                 .loginProcessingUrl("/login")
-                .loginPage("/login")                        
-                .failureUrl("/login?error") //エラー画面
-                .usernameParameter("username")  //login.html のusernameに相当
-                .passwordParameter("password")   //login.html のpasswordに相当
-                .defaultSuccessUrl("/menu/menu", true);  // /@GetMapping("/menu/menu")のコントローラーを作る必要あり。
+                .loginPage("/login")
+                .failureUrl("/login?error")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/menu/menu", true);
         http.csrf().disable();
     }
+    // ログイン認証に使用するユーザー情報を設定
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        // PasswordEncoder encoder = passwordEncoder();
         auth
                 .inMemoryAuthentication()
                 .withUser("User") // userを追加
--- src/test/java/com/example/spring_boot_memo/SecurityTest.java
+++ src/test/java/com/example/spring_boot_memo/SecurityTest.java
@@ -1,6 +1,11 @@
 package com.example.spring_boot_memo;
 
+import static org.junit.Assert.assertEquals;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.logout;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.security.NoSuchAlgorithmException;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
@@ -11,13 +16,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.logout;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import java.security.NoSuchAlgorithmException;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
@@ -28,45 +26,50 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class SecurityTest {
     @Autowired
     private MockMvc mvc;
-
     @Autowired
     PasswordEncoder passwordEncoder;
 
     @Test
-    @WithMockUser
     @DisplayName("認証成功後にアクセスページにログインアクセスできるか確認")
+    @WithMockUser
     public void whenGetCustomers_thenStatus200() throws Exception {
         mvc.perform(get("/menu/menu"))
                 .andExpect(status().isOk());
+
     }
 
     @Test
+    @DisplayName("認証成功後にログアウトのエンドポイントへリクエストすると、/logoutへリダイレクトする")
     @WithMockUser
-    @DisplayName("認証後にログアウトのエンドポイントへリクエストすると、/logoutへリダイレクトする")
     public void Logout() throws Exception {
-        mvc.perform(logout())
-        .andExpect(header().string("Location", "/login?logout"));
+        mvc.perform(logout("/logout"))
+        .andExpect(header().string("Location", "/login?logout"))
+      //  .andExpect(header().string("aa", "/login?logous"))
+      .andExpect(redirectedUrlTemplate("/login?logout"))
+        .andExpect(status().is3xxRedirection())
+        ;
+
     }
 
+
+
     @Test
-    @DisplayName("認証成功前のアクセス状況のチェック")
+    @DisplayName("認証されてないリクエストのとき、loginページにリダイレクトすることを検証")
     public void AccessCheckInNoAuth() throws Exception {
         mvc.perform(get("/check/OK"))
-                .andExpect(header().string("Location","http://localhost/login"));        // アクセスできないviewはloginへリダイレクト
-        mvc.perform(get("/check/OK"))
-                .andExpect(header().string("Location","http://localhost/login"));        // アクセスできないviewはloginへリダイレクト
+                .andExpect(header().string("Location","http://localhost/login"));
         mvc.perform(get("/check/not_access"))
-                .andExpect(header().string("Location","http://localhost/login"));        // アクセスできないviewはloginへリダイレクト
+                .andExpect(header().string("Location","http://localhost/login"));
         mvc.perform(get("/menu/menu"))
-                .andExpect(header().string("Location","http://localhost/login"));        // アクセスできないviewはloginへリダイレクト
+                .andExpect(header().string("Location","http://localhost/login"));
         mvc.perform(get("/login"))
-                .andExpect(status().isOk());         
-    }
+                .andExpect(status().isOk());
 
+    }
     @Test
     @DisplayName("比較-エンコード後の生のパスワードとストレージからのエンコードされたパスワード")
     public void passwordBCryptMatchCheck() throws NoSuchAlgorithmException{
-        String expected = "PASS"; 
+        String  expected = "PASS"; 
         String actual = passwordEncoder.encode(expected);
         Boolean isActual = passwordEncoder.matches(expected,actual);
         assertEquals(true, isActual);
